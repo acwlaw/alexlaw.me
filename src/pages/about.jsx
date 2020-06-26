b08d54158eca689ab113b96ac876cd4cd5aed1d9
Alex Law <alex.law96@gmail.com>
2020-06-26 13:39:28 -0700

    Add name on about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import './about.css';
 
 class About extends Component {
   state = {};
+
   render() {
     return (
       <div className="about-section" id="about">
-
+        <p id="nameText">
+          Alex
+          <br />
+          Law.
+        </p>
       </div>
-    )
+    );
   }
 }
 
