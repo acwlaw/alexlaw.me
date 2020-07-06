fee0c83ad1ea5a7d591988b701012d432b5365b6
Alex Law <alex.law96@gmail.com>
2020-07-06 12:41:53 -0700

    center align experience section

--- src/pages/experience.jsx
+++ src/pages/experience.jsx
@@ -7,44 +7,46 @@ class Experience extends Component {
   render() {
     return (
       <div className="experience-section" id="experience">
-        <ExperienceEntry
-          logo="amazon"
-          title="Amazon"
-          location="Vancouver, BC"
-          description="Software Development Engineer"
-          period="Aug 2020 - Present"
-        />
-        <ExperienceEntry
-          logo="apple"
-          logoRequiresPadding
-          title="Apple"
-          location="Cupertino, CA"
-          description="Software Engineer Intern"
-          period="May 2019 - Aug 2019"
-        />
-        <ExperienceEntry
-          logo="bench"
-          title="Bench Accounting"
-          location="Vancouver, BC"
-          description="Software Engineer Intern"
-          period="Jan 2019 - Apr 2019"
-        />
-        <ExperienceEntry
-          logo="rbc"
-          logoRequiresPadding
-          title="Royal Bank of Canada"
-          location="Toronto, ON"
-          description="iOS Developer Co-op"
-          period="Sept 2018 - Dec 2018"
-        />
-        <ExperienceEntry
-          logo="floka"
-          logoRequiresPadding
-          title="Flöka"
-          location="Vancouver, BC"
-          description="Software Engineer Intern"
-          period="Apr 2018 - Aug 2018"
-        />
+        <div>
+          <ExperienceEntry
+            logo="amazon"
+            title="Amazon"
+            location="Vancouver, BC"
+            description="Software Development Engineer"
+            period="Aug 2020 - Present"
+          />
+          <ExperienceEntry
+            logo="apple"
+            logoRequiresPadding
+            title="Apple"
+            location="Cupertino, CA"
+            description="Software Engineer Intern"
+            period="May 2019 - Aug 2019"
+          />
+          <ExperienceEntry
+            logo="bench"
+            title="Bench Accounting"
+            location="Vancouver, BC"
+            description="Software Engineer Intern"
+            period="Jan 2019 - Apr 2019"
+          />
+          <ExperienceEntry
+            logo="rbc"
+            logoRequiresPadding
+            title="Royal Bank of Canada"
+            location="Toronto, ON"
+            description="iOS Developer Co-op"
+            period="Sept 2018 - Dec 2018"
+          />
+          <ExperienceEntry
+            logo="floka"
+            logoRequiresPadding
+            title="Flöka"
+            location="Vancouver, BC"
+            description="Software Engineer Intern"
+            period="Apr 2018 - Aug 2018"
+          />
+        </div>
       </div>
     );
   }
