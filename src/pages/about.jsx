1758a9425d3d06ca6e67d74525a75457ea390894
Alex Law <alex.law96@gmail.com>
2020-07-07 12:11:18 -0700

    Proper positioning of name column

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -8,17 +8,17 @@ class About extends Component {
   render() {
     return (
       <div className="about-section" id="about">
-        <div id="nameColumn">
+        <div id="name-column">
           <div id="name-components">
             <svg width="420" height="40" viewBox="0 0 420 37" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M1 1H223.34M196.66 36H419M101.794 18.5H309.312" stroke="#E8DBC5" strokeWidth="4.5" strokeLinecap="round" strokeDasharray="4 16 14 4 16 14" />
             </svg>
-            <p id="nameText">
+            <p id="name-text">
               Alex
               <br />
               Law.
             </p>
-            <p id="subtitleText">
+            <p id="subtitle-text">
               Software Engineer
             </p>
             <svg width="420" height="40" viewBox="0 0 420 37" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -26,7 +26,7 @@ class About extends Component {
             </svg>
           </div>
         </div>
-        <div id="infoColumn">
+        <div id="info-column">
           <div id="info-components">
             <div id="headshot">
               {/*<img id="headshot-img" src={headshot} alt="headshot" width="500" />*/}
