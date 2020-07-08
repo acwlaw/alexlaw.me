3a580d56d1c36eaa6b9acf37819a6d3d3e9ee01a
Alex Law <alex.law96@gmail.com>
2020-07-08 14:49:06 -0700

    Updates

---
+++ precache-manifest.772fc4154965cf3063618e0cbf4392cb.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f79316bbd8dca2b3ce350d723e4d9fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63e3b00edad3706b33cb",
+    "url": "/static/css/2.dcba42a9.chunk.css"
+  },
+  {
+    "revision": "306a4d7b42939f20aaca",
+    "url": "/static/css/main.d7274e32.chunk.css"
+  },
+  {
+    "revision": "63e3b00edad3706b33cb",
+    "url": "/static/js/2.808c188c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.808c188c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "306a4d7b42939f20aaca",
+    "url": "/static/js/main.bfb0cf90.chunk.js"
+  },
+  {
+    "revision": "7d4a7932cfdcd47c370c",
+    "url": "/static/js/runtime-main.0128b0c7.js"
+  },
+  {
+    "revision": "f94ab6b8e19cae94b84a2935a18a4abe",
+    "url": "/static/media/Mermaid1001.f94ab6b8.ttf"
+  },
+  {
+    "revision": "644563f48ab5fe8e9082b64b2729b068",
+    "url": "/static/media/SF-Pro-Display-Bold.644563f4.otf"
+  },
+  {
+    "revision": "5810cdf5bc7db8d0090a6c46890ee33b",
+    "url": "/static/media/SF-Pro-Display-Regular.5810cdf5.otf"
+  },
+  {
+    "revision": "f9d6302c239d4012bd3551203a43cbcd",
+    "url": "/static/media/abode.f9d6302c.png"
+  },
+  {
+    "revision": "ef81cd43c0b24dd1759c8abd848a4c05",
+    "url": "/static/media/amazon.ef81cd43.png"
+  },
+  {
+    "revision": "13457c9db058dc44ad505e0d5ad8377c",
+    "url": "/static/media/bench.13457c9d.png"
+  },
+  {
+    "revision": "599086495fde11339cb82cd78de9a026",
+    "url": "/static/media/checkmates.59908649.png"
+  },
+  {
+    "revision": "ace595b54b61d9f71b9cd6c93d6db670",
+    "url": "/static/media/floka.ace595b5.png"
+  },
+  {
+    "revision": "855c637737fdc4206e8c505fed090fd8",
+    "url": "/static/media/headshot.855c6377.png"
+  },
+  {
+    "revision": "dd2634c72a2ee47fdbe5b6478b037092",
+    "url": "/static/media/personalsite.dd2634c7.png"
+  },
+  {
+    "revision": "5baeb89edf23c3365d5b2bbee271d978",
+    "url": "/static/media/rbc.5baeb89e.png"
+  },
+  {
+    "revision": "3726fe09a84e5ac767ea7a9ab281c51f",
+    "url": "/static/media/statsonsight.3726fe09.png"
+  }
+]);
